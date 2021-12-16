2a74489c47b40aff86171c75f5a5e2a733d9a395
Oleg <fed.olegator1@gmail.com>
2021-12-17 03:53:01 +1000

    Comment unnecessery logs

--- scripts/main.js
+++ scripts/main.js
@@ -16,7 +16,7 @@ function SetGrid() {
 
     SetTeams();
     var numberOfTeams = teamNames.length;
-    console.log((numberOfTeams & (numberOfTeams - 1)) == 0);
+    //console.log((numberOfTeams & (numberOfTeams - 1)) == 0);
 
     if ((numberOfTeams & (numberOfTeams - 1)) == 0) {
 
@@ -31,7 +31,7 @@ function SetGrid() {
         for (var j = 0; j < numberOfTeams / 2; j++){
             //console.log(numberOfTeams / Math.pow(2, i+1));
             //document.getElementsByName("round"+i)[0].innerHTML += '<div class="game-node"><input type="text" class="name-text" id="name'+ (i*100 + 2*j) +'"> <input type="text" class="name-text" id="name'+ (i*100 + 2*j+1) +'"> </div>';
-            console.log(2*j, 2*j+1);
+            //console.log(2*j, 2*j+1);
             var elementsToFill1 = document.getElementById("name"+(2*j));
             var elementsToFill2 = document.getElementById("name"+(2*j+1));
             //console.log(document.getElementById("name"+(2*j)));
