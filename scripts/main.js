d682cfc990433fc6459586185c612c9718a8749c
Oleg <fed.olegator1@gmail.com>
2021-12-17 03:41:39 +1000

    Implemented teams names to the grid

--- scripts/main.js
+++ scripts/main.js
@@ -24,9 +24,19 @@ function SetGrid() {
             //console.log(numberOfTeams / Math.pow(2, i+1));
             document.getElementById("bracket-area2").innerHTML += '<div class="col" name="round' + i + '"></div>';
             for (var j = 0; j < numberOfTeams / Math.pow(2, i+1); j++){
-                console.log(numberOfTeams / Math.pow(2, i+1));
-                document.getElementsByName("round"+i)[0].innerHTML += '<div class="game-node"><input type="text" class="name-text" name="name'+ (i*10 + j) +'"> <input type="text" class="name-text" name="name'+ (i*10 + (j+1)) +'"> </div>'
+                //console.log(numberOfTeams / Math.pow(2, i+1));
+                document.getElementsByName("round"+i)[0].innerHTML += '<div class="game-node"><input type="text" class="name-text" id="name'+ (i*100 + 2*j) +'"> <input type="text" class="name-text" id="name'+ (i*100 + 2*j+1) +'"> </div>';
             }
+        }
+        for (var j = 0; j < numberOfTeams / 2; j++){
+            //console.log(numberOfTeams / Math.pow(2, i+1));
+            //document.getElementsByName("round"+i)[0].innerHTML += '<div class="game-node"><input type="text" class="name-text" id="name'+ (i*100 + 2*j) +'"> <input type="text" class="name-text" id="name'+ (i*100 + 2*j+1) +'"> </div>';
+            console.log(2*j, 2*j+1);
+            var elementsToFill1 = document.getElementById("name"+(2*j));
+            var elementsToFill2 = document.getElementById("name"+(2*j+1));
+            //console.log(document.getElementById("name"+(2*j)));
+            elementsToFill1.value = teamNames[2*j];
+            elementsToFill2.value = teamNames[2*j+1];
         }
     }
     else {
